6ef8fde2e1103a1ac98208cd284640f00b840758
Hoang Nguyen <tranhoangcore@gmail.com>
2021-03-31 17:15:10 +0700

    Isomorphic Strings

--- C++/0205-isomorphic-strings.cpp
+++ C++/0205-isomorphic-strings.cpp
@@ -1,7 +1,31 @@
 class Solution
 {
 public:
+
+    bool findVal(unordered_map<char, char> &hashtable, const char &c){
+        for(auto i = hashtable.begin(); i != hashtable.end(); i++){
+            if(i->second == c)
+                return true;
+        }
+        return false;
+    }
+
     bool isIsomorphic(string s, string t)
     {
+        if(s.length() != t.length())
+            return false;
+        unordered_map<char,char> hashtable;
+
+        for(int i = 0; i < s.length(); i++){
+            if(hashtable.find(s[i])  == hashtable.end()){
+                if(findVal(hashtable, t[i]))
+                    return false;
+                else
+                    hashtable[s[i]] = t[i];
+            }
+            else if(hashtable[s[i]] != t[i])
+                return false;
+        }
+        return true;
     }
 };
