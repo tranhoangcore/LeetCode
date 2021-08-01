96876a81a1727e2ff2fc7a0a360718e233cc512e
Hoang Nguyen <tranhoangcore@gmail.com>
2021-08-01 18:52:25 +0700

    1480. Running Sum of 1d Array

---
+++ C++/0289-game-of-life.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+      int m = board.size(), n = m ? board[0].size : 0;
+      for(int i = 0; i < m; i++){
+        int count = 0; 
+        for(int I = max( i-1; 0); I < min(i+2, m); I++){
+        2 ''}
+      }
+    }
+};
