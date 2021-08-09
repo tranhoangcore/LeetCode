b4fbbf30f6a919cf734852f5deb9b437e997e701
Hoang Nguyen <tranhoangcore@gmail.com>
2021-08-09 07:53:20 +0700

    1281. Subtract the Product and Sum of Digits of an Integer

--- C++/1512-number-of-good-pairs.cpp
+++ C++/1512-number-of-good-pairs.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
       int result = 0;
-      unoredered_map<int, int> count;
+      unordered_map<int, int> count;
       for(int i : nums){
         result += count[i]++;
       }
