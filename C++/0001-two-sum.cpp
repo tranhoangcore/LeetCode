ffe76dbfe91cc87a82a92ade0fff832425779f2b
Nguyen Tran Hoang <tranhoangcore@gmail.com>
2023-05-08 18:21:05 +0700

    Update

--- C++/0001-two-sum.cpp
+++ C++/0001-two-sum.cpp
@@ -1,21 +1,17 @@
-class Solution
-{
+class Solution {
 public:
-    vector<int> twoSum(vector<int> &nums, int target)
-    {
-        vector<int> result;
-        unordered_map<int, int> hashtable;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            int numberFind = target - nums[i];
-            if (hashtable.count(numberFind) > 0)
-            {
-                result.push_back(hashtable[numberFind]);
-                result.push_back(i);
-                break;
-            }
-            hashtable[nums[i]] = i;
-        }
-        return result;
+  vector<int> twoSum(vector<int> &nums, int target) {
+    vector<int> result;
+    unordered_map<int, int> hashtable;
+    for (int i = 0; i < nums.size(); i++) {
+      int numberFind = target - nums[i];
+      if (hashtable.count(numberFind) > 0) {
+        result.push_back(hashtable[numberFind]);
+        result.push_back(i);
+        break;
+      }
+      hashtable[nums[i]] = i;
     }
+    return result;
+  }
 };
